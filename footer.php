723c70e8c1e9d82df302030a996612c00e982c65
Scott May <smay44@gmail.com>
2019-07-05 08:29:14 -0400

    Set up breakpoints for footer widgets

--- footer.php
+++ footer.php
@@ -17,20 +17,26 @@
 		<div class="container-fluid">
 			<div class="row">
 				<?php if(is_active_sidebar('footer-1')): ?>
-					<div id="footer-section-1" class="col-12">
+					<div id="footer-section-1" class="col-12 col-md-6 col-xl-5 footer-section">
 						<?php dynamic_sidebar('footer-1'); ?>
 					</div>
-				<?php endif ?>
-				<?php if(is_active_sidebar('footer-2')): ?>
-					<div id="footer-section-2" class="col-12">
-						<?php dynamic_sidebar('footer-2'); ?>
+				<?php endif; ?>
+				<?php if(is_active_sidebar('footer-2') || is_active_sidebar('footer-3') ): ?>
+					<div class="footer-section-container container-fluid col-12 col-md-6 col-xl-7">
+						<div class="row">
+							<?php if(is_active_sidebar('footer-2')): ?>
+								<div id="footer-section-2" class="col-12 col-xl-6 footer-section">
+									<?php dynamic_sidebar('footer-2'); ?>
+								</div>
+							<?php endif; ?>
+							<?php if(is_active_sidebar('footer-3')): ?>
+								<div id="footer-section-3" class="col-12 col-xl-6 footer-section">
+									<?php dynamic_sidebar('footer-3'); ?>
+								</div>
+							<?php endif; ?>
+						</div>
 					</div>
-				<?php endif ?>
-				<?php if(is_active_sidebar('footer-3')): ?>
-					<div id="footer-section-3" class="col-12">
-						<?php dynamic_sidebar('footer-3'); ?>
-					</div>
-				<?php endif ?>
+				<?php endif; ?>
 			</div>
 
 	</footer><!-- #colophon -->
